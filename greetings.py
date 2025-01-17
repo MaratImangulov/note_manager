95f9553c12cfa055228b1af65bc47d02d8785f8c
MaratImangulov <Iadelya@list.ru>
2025-01-17 16:19:40 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,15 @@
+
+
+# Переменные:
+username = 'marat'
+title = 'homework'
+content = '1 modul'
+status = 'in work'
+created_date = '17-01-2025'
+issue_date = '1-03-2025'
+print('Имя пользователя:', username)
+print('Заголовок заметки:',title)
+print('Описание заметки:',content)
+print('Статус заметки:',status)
+print('Дата создания заметки:',created_date)
+print('Дата истечения заметки:',issue_date)
