b51d54f9f413bc3d7a91909d08a3464ecbb7f095
MaratImangulov <Iadelya@list.ru>
2025-01-20 16:51:08 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,18 @@
+username = input('Введите ваше имя: ')
+title = input('Введите заголовок заметки: ')
+content = input('Введите описание заметки: ')
+status = input('Введите статус заметки:  ')
+created_date = input('Введите дату создания (дд:мм:гггг): ')
+issue_date = input('Введите дату оканчания заметки (дд:мм:гггг): ')
+
+title1 = input('Введите название заголовка: ')
+title2 = input('Введите название заголовка: ')
+title3 = input('Введите название заголовка: ')
+all_title = [title1, title2, title3]
+print('Имя пользователя:', username)
+print('Заголовок заметки:',title)
+print('Описание заметки:',content)
+print('Статус заметки:',status)
+print('Дата создания заметки:',created_date)
+print('Дата истечения aзаметки:',issue_date)
+print(all_title)
